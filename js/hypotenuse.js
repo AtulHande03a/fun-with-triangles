7f1f3841f62ecb26f7cf6dd953efa4ba0da47bb7
Atul Kumar Hande <handeatul03a@gmail.com>
2021-09-13 12:04:28 +0530

    added hypotenuse html & css

---
+++ js/hypotenuse.js
@@ -0,0 +1,4 @@
+const base = document.querySelector(".base");
+const height = document.querySelector(".height");
+const calculateBtn = document.querySelector("#calculate");
+const message = document.querySelector(".message");
